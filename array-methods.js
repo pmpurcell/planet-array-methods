b92cda7b5cefe7c913a6b98e182c82d294e199f7
pmpurcell93 <maddenpurcell@gmail.com>
2021-08-26 13:32:31 -0500

    Completed planets challenge

--- array-methods.js
+++ array-methods.js
@@ -1,21 +1,28 @@
-const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+const planets = [
+  "mercury",
+  "venus",
+  "earth",
+  "mars",
+  "jupiter",
+  "saturn",
+  "uranus",
+  "neptune",
+];
 
 /*
     Use the forEach method to add the name of each planet
     to a section element in your HTML with an id of "planets".
     Use string templates to construct the DOM elements.
 */
-const planetEl = document.getElementById("planets")
+const planetEl = document.getElementById("planets");
 
 const planetBuilder = (array) => {
-    array.forEach(arrayObj => {
-    
+  array.forEach((arrayObj) => {
     planetEl.innerHTML += `
     <h1>${arrayObj}</h1>
-    `
-        
-    });
-}
+    `;
+  });
+};
 
 // planetBuilder(planets);
 /*
@@ -26,11 +33,11 @@ const planetBuilder = (array) => {
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 */
 
-const capitalPlanets = planets.map(planet => {
-    return planet[0].toUpperCase() + planet.slice(1);
-})
+const capitalPlanets = planets.map((planet) => {
+  return planet[0].toUpperCase() + planet.slice(1);
+});
 
-planetBuilder(capitalPlanets);
+// planetBuilder(capitalPlanets);
 /*
     Use the filter method to create a new array that
     contains planets with the letter 'e'. Use the `includes()`
@@ -38,3 +45,17 @@ planetBuilder(capitalPlanets);
 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 */
+
+const planetsWithE = capitalPlanets.filter((planet) => {
+  let hasLetterE = false;
+
+  if (planet.includes("e") || planet.includes("E")) {
+    hasLetterE = true;
+  }
+
+  return hasLetterE;
+});
+
+console.log(planetsWithE);
+
+planetBuilder(planetsWithE);
